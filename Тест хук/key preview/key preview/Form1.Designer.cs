b89d88486767d23369318e3714d8d1ec3259ad31
ProteinR <proteinr@gmail.com>
2017-11-01 02:06:16 +0200

    Combinations Add
    Add hook on combination like ctrl+c, ctrl+v and ctrl+v+1

--- Тест хук/key preview/key preview/Form1.Designer.cs
+++ Тест хук/key preview/key preview/Form1.Designer.cs
@@ -24,25 +24,46 @@ namespace key_preview {
 		/// </summary>
 		private void InitializeComponent() {
             this.lstLog = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lstLog
             // 
-            this.lstLog.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lstLog.FormattingEnabled = true;
             this.lstLog.ItemHeight = 20;
             this.lstLog.Location = new System.Drawing.Point(0, 0);
             this.lstLog.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.lstLog.Name = "lstLog";
-            this.lstLog.Size = new System.Drawing.Size(438, 409);
+            this.lstLog.Size = new System.Drawing.Size(438, 404);
             this.lstLog.TabIndex = 0;
             this.lstLog.SelectedIndexChanged += new System.EventHandler(this.lstLog_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(462, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 385);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 413);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(438, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(438, 409);
+            this.ClientSize = new System.Drawing.Size(763, 473);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.lstLog);
             this.KeyPreview = true;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -50,12 +71,15 @@ namespace key_preview {
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.ListBox lstLog;
-	}
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
 }
 
--- Тест хук/key preview/key preview/Form1.cs
+++ Тест хук/key preview/key preview/Form1.cs
@@ -7,56 +7,138 @@ using System.Text;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using Utilities;
+using System.Collections.Generic;
 
 namespace key_preview {
 	public partial class Form1 : Form {
 		globalKeyboardHook gkh = new globalKeyboardHook();
+        
+        bool C = false;
+        bool V = false;
+        bool CTRL = false;
+        List<string> clipboardList = new List<String>(); //список с буфером
+        String[] clipboard = new String[10]; //массив с содержимым буфера обмена
+        int clipCount = 0; //кол-во элементов в массиве
 
-		public Form1() {
+        public Form1() {
 			InitializeComponent();
 		}
 
         private void Form1_Load(object sender, EventArgs e) {
-
+            
             gkh.HookedKeys.Add(Keys.A);
             gkh.HookedKeys.Add(Keys.B);
+            gkh.HookedKeys.Add(Keys.C); // - C
+            gkh.HookedKeys.Add(Keys.V);
+            gkh.HookedKeys.Add(Keys.LControlKey); //левый Ctrl
+
+            //ряд цифр
+            gkh.HookedKeys.Add(Keys.D1);
+            gkh.HookedKeys.Add(Keys.D2);
+            gkh.HookedKeys.Add(Keys.D3);
+            gkh.HookedKeys.Add(Keys.D4);
+            gkh.HookedKeys.Add(Keys.D5);
+            gkh.HookedKeys.Add(Keys.D6);
+            gkh.HookedKeys.Add(Keys.D7);
+            gkh.HookedKeys.Add(Keys.D8);
+            gkh.HookedKeys.Add(Keys.D9);
+
             gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
             gkh.KeyUp += new KeyEventHandler(gkh_KeyUp);
+            
+            //ненужный массив
+            Char[] chars = {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
+                'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Y',
+                'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+            };
 
-            //int[] arr1Line = { 1, 2, 3, 4, 5 };
-            Char[] chars = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7','8', '9', '0' };
-            for (int i=0; i<35; i++)
-            {
-                //gkh.HookedKeys.Add(Keys.chars[i]); -- не работает подстановка из массива
-            }
+            //for (int i=0; i<32; i++)
+            //{
+            //    Char symbol = chars[i];
+            //    gkh.HookedKeys.Add(Keys.symbol); 
+            //}
 		}
         protected override void OnKeyDown(KeyEventArgs e)
         {
-            if (e.Control && e.KeyCode == Keys.C)
-                MessageBox.Show("you have pressed Ctrl + C");
-            else if (e.Control && e.KeyCode == Keys.V)
-                MessageBox.Show("You have pressed Ctrl + V");
-            //else if (e.Control && e.KeyCode == Keys.V && Keys.(preg_match('[0-9]'));
+            //вывод сообщения при нажатии комбинации
+            //textBox1.Text = e.ToString();
         }
         void gkh_KeyDown(object sender, KeyEventArgs e)
         {
             lstLog.Items.Add("Down\t" + e.KeyCode.ToString());
-            //if (e.Control && e.KeyCode == Keys.A)
-            //    MessageBox.Show("you have pressed Ctrl + A");
-            //else if (e.Control && e.Shift && e.KeyCode == Keys.A)
-            //    MessageBox.Show("You have pressed Ctrl + Shift + A");
-            //e.Handled = true;
+            
+            //устанавливаем флаги при нажатии 
+            if (e.KeyCode == Keys.LControlKey)
+            { 
+                CTRL = true;
+            }
+            else if (e.KeyCode == Keys.C)
+            { 
+                C = true;
+            }
+            else if (e.KeyCode == Keys.V)
+            { 
+                V = true;
+            }
+
+
+            //проверка - нажата ли комбинация
+            if (CTRL && C)
+            {
+                textBox1.Text += "Ctrl+C pressed";
+                String lastClipboar = Clipboard.GetText(); //записали значение буфера в переменную
+                //Array.add(clipboard, lastClipboar);  //добавляем значение буфера в конец 
+                //MessageBox.Show("Ctrl+C");
+
+                if (clipCount < 9) //если есть место в буфере - записали в конец крайнее копирование
+                {
+                    clipboard[clipCount + 1] = lastClipboar; 
+                }
+
+            }
+            //else if (CTRL && V) //простая комбинация ctrl+v
+            //{
+            //    textBox1.Text += "Ctrl+V pressed";
+            //}
+            else if (CTRL && V && e.KeyCode == Keys.D1)
+            {
+                textBox1.Text += "Ctrl+V - cупер комбинация была нажата";
+                textBox1.Text += "Ctrl+V" + e.KeyCode.ToString() +  " pressed";
+            }
+
+            e.Handled = false; //Если false, то отправляем событие дальше
         }
 
         void gkh_KeyUp(object sender, KeyEventArgs e) {
 			lstLog.Items.Add("Up\t" + e.KeyCode.ToString());
-			e.Handled = true;
-            
+
+            //убираем флаги при отжатии
+            if (e.KeyCode == Keys.C)
+            {
+                C = false;
+            }
+            else if (e.KeyCode == Keys.LControlKey)
+            {
+                CTRL = false;
+            }
+            else if (e.KeyCode == Keys.V)
+            {
+                V = false;
+            }
+
+            e.Handled = false;
         }  
 
         private void lstLog_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lstLog.Items.Clear();
+            textBox1.Clear();
         }
     }
 }
