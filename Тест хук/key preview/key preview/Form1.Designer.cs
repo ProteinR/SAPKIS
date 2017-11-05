071d2b283643cb90b5cb116037a95523f8f47ded
ProteinR <proteinr@gmail.com>
2017-11-05 18:09:53 +0200

    Clipboard 0.1
    Combination RCTRL + nums added

--- Тест хук/key preview/key preview/Form1.Designer.cs
+++ Тест хук/key preview/key preview/Form1.Designer.cs
@@ -23,10 +23,13 @@ namespace key_preview {
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lstLog = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lstLog
@@ -45,14 +48,14 @@ namespace key_preview {
             this.textBox1.Location = new System.Drawing.Point(462, 12);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(268, 385);
+            this.textBox1.Size = new System.Drawing.Size(268, 392);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(0, 413);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(438, 48);
+            this.button1.Size = new System.Drawing.Size(730, 48);
             this.button1.TabIndex = 2;
             this.button1.Text = "Очистить";
             this.button1.UseVisualStyleBackColor = true;
@@ -63,15 +66,37 @@ namespace key_preview {
             this.textBox2.Location = new System.Drawing.Point(736, 12);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(268, 385);
+            this.textBox2.Size = new System.Drawing.Size(268, 392);
             this.textBox2.TabIndex = 3;
-            this.textBox2.Text = "Первая строка\r\nВторая строка\r\nТретья строка\r\nЧетвёртая строка\r\nПятая строка\r\n";
+            this.textBox2.Text = resources.GetString("textBox2.Text");
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1011, 13);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(299, 391);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1016, 414);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(294, 47);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Вывод массива с историей";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1022, 473);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1322, 512);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -92,6 +117,8 @@ namespace key_preview {
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
     }
 }
 
