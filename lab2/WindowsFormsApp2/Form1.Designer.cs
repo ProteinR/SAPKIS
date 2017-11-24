4a825988dd023ae20ee35be6de32cecd9b4d6c15
Dmitriy <proteinr@gmail.com>
2017-11-24 22:42:04 +0200

    Lab 2 file system

---
+++ lab2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.output.Font = new System.Drawing.Font("Lucida Console", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.output.Location = new System.Drawing.Point(18, 31);
+            this.output.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(1425, 471);
+            this.output.TabIndex = 0;
+            this.output.Text = "";
+            this.output.TextChanged += new System.EventHandler(this.output_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1461, 542);
+            this.Controls.Add(this.output);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox output;
+    }
+}
+
---
+++ lab2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+    
+        const String _MARGIN = "    ";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           //get initial root dir
+            System.IO.DirectoryInfo rootDir = System.IO.Directory.
+                    CreateDirectory(System.IO.Directory.GetCurrentDirectory());
+
+            WalkDirectoryTree(rootDir, "");//margin is empty for first call
+        }
+
+        void WalkDirectoryTree(System.IO.DirectoryInfo root, String margin)
+            {
+                System.IO.FileInfo[] files = null;
+                System.IO.DirectoryInfo[] subDirs = null;
+
+            try
+            {
+                files = root.GetFiles("*.*");
+                output.AppendText(margin + "\\"+root.Name+"\n");
+            }
+            catch (UnauthorizedAccessException e) { output.AppendText(e.Message); }
+            catch (System.IO.DirectoryNotFoundException e) { output.AppendText(e.Message);}
+
+                if (files != null)
+                {
+                    //FILES
+                    foreach (System.IO.FileInfo fi in files)
+                    {
+                        //Print each filename with margin
+                        output.AppendText(margin + _MARGIN + "|*" + fi.Name + "\n");
+                    }
+
+                    subDirs = root.GetDirectories();
+
+                    //DIRS
+
+                    foreach (System.IO.DirectoryInfo dirInfo in subDirs)
+                    {
+                        // Rec. call for each sub dir with increase margin.
+                        WalkDirectoryTree(dirInfo, margin+ _MARGIN);
+                    }
+                }
+            }
+
+        private void output_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
